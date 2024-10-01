4467be3c010fb46b0e63967ce46d8d42b041cc61
Mark Golino <markvpr@gmail.com>
2024-10-01 15:16:41 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "Windows-MPG-Server" {
-#  ami           = var.ami
-#  instance_type = var.instance_type
-  ami           = "ami-03db23f7d74959cbb"
-  instance_type = "t2.small"
+  ami           = var.ami
+  instance_type = var.instance_type
+#  ami           = "ami-03db23f7d74959cbb"
+#  instance_type = "t2.small"
 
   credit_specification {
     cpu_credits = "unlimited"
