bc967483a00ca094720764bbbae353d2ef3e7f6d
Mark Golino <markvpr@gmail.com>
2024-10-01 14:24:32 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,10 @@
 variable "instance_type" {
+  type = string
   description = "Type of EC2 instance to provision"
   default     = "t2.medium"
 }
+
+variable "ami" {
+  type = string
+  default = "ami-03db23f7d74959cbb"
+  }
