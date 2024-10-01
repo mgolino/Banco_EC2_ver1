841764688806246c6cb7a481e4c20735ed29fdc9
Mark Golino <markvpr@gmail.com>
2024-10-01 14:39:41 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,6 @@ resource "aws_instance" "Windows-MPG-Server" {
   ami           = var.ami
   instance_type = var.instance_type
 
-  network_interface {
-    network_interface_id = var.network_interface_id
-    device_index         = 0
-  }
-
   credit_specification {
     cpu_credits = "unlimited"
   }
