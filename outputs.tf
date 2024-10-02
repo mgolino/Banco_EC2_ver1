bfc4ce6bc75de0dbeacd62fda41ea18ea4cbd787
Mark Golino <markvpr@gmail.com>
2024-10-02 15:13:59 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -4,6 +4,7 @@ output "instance_ami" {
 
 output "instance_ip_addr" {
   value = aws_instance.Windows-MPG-Server.private_ip
+  description = "The private IP address of the server"
 }
 
 output "instance_arn" {
