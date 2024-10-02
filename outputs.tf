40b8f873e178248b8881180ec990ba315adc84a2
Mark Golino <markvpr@gmail.com>
2024-10-02 15:03:18 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,12 @@
-/* output "instance_ami" {
+output "instance_ami" {
   value = aws_instance.Windows-MPG-Server.ami
 }
 
-output "instance_arn" {
-  value = aws_instance.Windows-MPG-Server.arn
+output "instance_ip_addr" {
+  value = aws_instance.Windows-MPG-Server_ip
 }
-*/
+
+/*output "instance_arn" {
+  value = aws_instance.Windows-MPG-Server.arn
+}*/
+
