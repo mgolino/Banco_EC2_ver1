c99fc43bfcb1821e29f2a10235a973854397ec31
Mark Golino <markvpr@gmail.com>
2024-10-04 15:02:34 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "Windows-MPG-Server" {
   # count = 1 # Number of instances to create
   ami           = var.ami
   instance_type = var.instance_type
+  subnet_id = "subnet-0baaac8c622257ef8"
 #  ami           = "ami-03db23f7d74959cbb"
 #  instance_type = "t2.small"
 
