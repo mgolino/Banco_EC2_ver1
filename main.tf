68a215fe8d4c6fca1b659b6fdba9b670e351c598
Mark Golino <markvpr@gmail.com>
2024-10-07 16:23:39 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,15 @@ resource "aws_instance" "Windows-MPG-Server" {
   # count = 1 # Number of instances to create
   ami           = var.ami
   instance_type = var.instance_type
-#  root_volume_encryption_enabled = true
-#  volume_encryption_key = "arn:aws:kms:us-east-1:222634369789:key/mrk-435ef7d1448341e784453f666cf0f1da"
   subnet_id = "subnet-0baaac8c622257ef8"
 #  ami           = "ami-03db23f7d74959cbb"
 #  instance_type = "t2.small"
-
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = 200
+    encrypted   = true
+    kms_key_id  = aws_ebs_default_kms_key.mrk-435ef7d1448341e784453f666cf0f1da.key_arn
+  }
   credit_specification {
     cpu_credits = "unlimited"
   }
@@ -19,6 +22,6 @@ resource "aws_instance" "Windows-MPG-Server" {
   }
 }
 
-resource "aws_ebs_default_kms_key" "default" { 
-key_arn = arn:aws:kms:us-east-1:222634369789:key/mrk-435ef7d1448341e784453f666cf0f1da 
-}
+
+# key_arn = arn:aws:kms:us-east-1:222634369789:key/mrk-435ef7d1448341e784453f666cf0f1da 
+
