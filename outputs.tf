74674c548bc771f627d212e3e49c8af62b12321b
Mark Golino <markvpr@gmail.com>
2024-10-07 15:24:40 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -15,3 +15,12 @@ output "instance_arn" {
   value = aws_instance.Windows-MPG-Server.arn
 }
 
+output "instance_id" {
+  description = "ID of EC2 instance"
+  value = aws_instance.Windows-MPG-Server.id
+}
+
+/* output "instance_public_ip" {
+  description = "The public IP address of the server"
+  value = aws_instance.Windows-MPG-Server.public_ip
+}*/
