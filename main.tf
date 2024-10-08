5ca70f3d26d0ae17ff80ed075e7499cee843f7b4
Mark Golino <markvpr@gmail.com>
2024-10-08 17:02:06 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,12 @@
 
 data "aws_ebs_default_kms_key" "ebs_kms_key" {}
 
-data "aws_security_group" "sg-02187bbed09dbd891" {
+/* data "aws_security_group" "sg-02187bbed09dbd891" {
     name = "MPG-SG"
+  }*/
+
+data "aws_security_group" "MPG-SG" {
+    id = "sg-02187bbed09dbd891"
   }
 
 
@@ -26,8 +30,9 @@ resource "aws_instance" "Windows-MPG-Server" {
   ami = data.aws_ami.windows.id    # This is referenced from above - data "aws_ami"
   instance_type = var.instance_type    # This var is in the variables.tf file
   key_name = "MPG-Server"    # This will be the server key created to launch all EC2's
-  vpc_security_group_ids = [data.aws_security_group.sg-02187bbed09dbd891.id]    # This is referenced from above - data "aws_security_group"
-  subnet_id = "subnet-0ba3d6a676259e6f7"  # Change this to your subnet
+  # vpc_security_group_ids = [data.aws_security_group.sg-02187bbed09dbd891.id]    # This is referenced from above - data "aws_security_group"
+  vpc_security_group_ids = [data.aws_security_group.MPG-SG.id]    # This is referenced from above - data "aws_security_group"
+subnet_id = "subnet-0ba3d6a676259e6f7"  # Change this to your subnet
   availability_zone = "us-east-1b"    # Change this to your preferred AZ
 #  ami           = "ami-03db23f7d74959cbb"  # Use this and comment out the other line to hard code
 #  instance_type = "t2.small"    # Use this and comment out the other line to hard code
